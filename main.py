e9dfd46e9088b82b8c1b678e5f3996f054525757
obarafernando <obara.fernando@gmail.com>
2018-12-06 12:00:09 -0200

    Removed unnecessary prints

--- main.py
+++ main.py
@@ -27,20 +27,20 @@ def main():
         current_characters = characters
         while len(current_characters) > 1:
             random_question = randomIndex(current_characters)
-            print(random_question)
+            #print(random_question)
             random_character = random.choice(current_characters)
             answer = answer_to_bool(input(randomQuestion(random_character, random_question) + " " + yes_or_no()))
             compare_string = comparation_string(random_question, answer, random_character)
             current_characters = list(filter(lambda x: x[random_question] == compare_string if answer \
                     else x[random_question] != compare_string, current_characters))
-            for character in current_characters:
-                print(character)
+            #for character in current_characters:
+                #print(character)
 
         if not current_characters:
             print("do not know this character")
         else:
             answer = answer_to_bool(input(randomQuestion(current_characters[0], 0) + " " + yes_or_no()))
-            print(current_characters[0] if answer else "do not know this character")
+            print('Ez Boys' if answer else "do not know this character")
 
         answer = answer_to_bool(input("another one? " + yes_or_no()))
         another_one = answer
